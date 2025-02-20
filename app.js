65d0a2ff9268fd6960369a77e421dd6272b520bb
Renan Matthew Fajardo <renanmatthew206@gmail.com>
2025-02-20 23:02:17 +0800

    chore: created and clean the code and apply game logic

--- app.js
+++ app.js
@@ -1,114 +1,117 @@
 let player_one = 0;
 let player_two = 0;
-const tile_wrapper = ['', '', '', '', '', '', '', '', ''];
+const tile_wrapper = ["", "", "", "", "", "", "", "", ""];
 const column_count = 3;
-const [tile_one, tile_two, tile_three, tile_four, tile_five, tile_six, tile_seven, tile_eight, tile_nine] = tile_wrapper;
+const [
+  tile_one,
+  tile_two,
+  tile_three,
+  tile_four,
+  tile_five,
+  tile_six,
+  tile_seven,
+  tile_eight,
+  tile_nine,
+] = tile_wrapper;
 
-const startGame = (player,key) => {
-
-    const getScore = (player) => {
-        if (player === 'player_one'){
-            player_one += 1;
-        } else {
-            player_two += 1;
-        }
+const startGame = (player, key) => {
+  const getScore = (player) => {
+    if (player === "player_one") {
+      player_one += 1;
+    } else {
+      player_two += 1;
     }
+  };
 
-    const resetScore = () => {
-        player_one = 0;
-        player_two = 0;
+  const resetScore = () => {
+    player_one = 0;
+    player_two = 0;
+  };
+
+  const resetGame = () => {
+    tile_wrapper = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  };
+
+  const gameLogic = (player, key) => {
+    // [...document.querySelectorAll(".box-wrapper .box")].map(tile => tile.classList.contains("cross") ? 1 : tile.classList.contains("circle") ? 2 : 0)
+  };
+
+  const gameLogicChecker = () => {
+    if (tile_one === tile_two && tile_two === tile_three && tile_one !== 0) {
+      // alert(playerVerifier(key));
+    } else if (
+      tile_four === tile_five &&
+      tile_five === tile_six &&
+      tile_four !== 0
+    ) {
+      // alert(playerVerifier(key));
+    } else if (
+      tile_seven === tile_eight &&
+      tile_eight === tile_nine &&
+      tile_seven !== 0
+    ) {
+      // alert(playerVerifier(key));
+    } else {
+      return alert("None");
     }
+  };
 
-    const resetGame = () => {
-        tile_wrapper = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const playerVerifier = (key) => {
+    var name = "";
+    if (key === "x") {
+      name = `${player} won`;
+    } else {
+      name = "AI won";
     }
- 
+    console.log(name);
+    return name;
+  };
 
-    const gameLogic = (player, key) =>{
-        // [...document.querySelectorAll(".box-wrapper .box")].map(tile => tile.classList.contains("cross") ? 1 : tile.classList.contains("circle") ? 2 : 0)
-    }
+  return { gameLogicChecker };
+};
 
+const game = startGame("Itik", "x");
 
-    const gameLogicChecker = () => {
+const box_wrapper = document.querySelector(".box-wrapper");
 
-        if ((tile_one === tile_two) && (tile_two === tile_three) && tile_one !== 0){
-            // alert(playerVerifier(key));
-        } else if((tile_four === tile_five) && (tile_five === tile_six) && tile_four !== 0){
-            // alert(playerVerifier(key));
-        } else if((tile_seven === tile_eight) && (tile_eight === tile_nine) && tile_seven !== 0){
-            // alert(playerVerifier(key));
-        }else {
-            return alert('None');
-        }
-    }
+const box_wrapper = document.querySelector(".box-wrapper");
 
-    const playerVerifier = (key) => {
-        var name = '';
-        if (key === 'x'){
-            name = `${player} won`
-        } else {
-            name = 'AI won'
-        }
-        console.log(name);
-        return name;
-    }
-
-    return {gameLogicChecker};
-
-}
-
-const game = startGame('Itik','x');
-
-
-
-
-
-
-const box_wrapper = document.querySelector('.box-wrapper');
-
-
-for (var i = 1; i <= 9; i++){
-    const box = document.createElement('div');
-    box.className = `box box-${i}`;
-    box_wrapper.appendChild(box);
+for (var i = 1; i <= 9; i++) {
+  const box = document.createElement("div");
+  box.className = `box box-${i}`;
+  box_wrapper.appendChild(box);
 }
 
 document.querySelectorAll(".box").forEach((box) => {
-    box.addEventListener("click", (e) => {
-        console.log(e);
-        if (!box.classList.contains("circle") && !box.classList.contains("cross")){
-                box.className += ' cross';
-                game.gameLogicChecker();
-                setTimeout(randomBot, 300);
-        } else {
-            alert('Please select other box');
-        }
-    })
-})
+  box.addEventListener("click", (e) => {
+    console.log(e);
+    if (!box.classList.contains("circle") && !box.classList.contains("cross")) {
+      box.className += " cross";
+      game.gameLogicChecker();
+      setTimeout(randomBot, 300);
+    } else {
+      alert("Please select other box");
+    }
+  });
+});
 
 function randomBot() {
-    let emptyBoxes = Array.from(document.querySelectorAll(".box")).filter(box => 
-        !box.classList.contains("circle") && !box.classList.contains("cross")
-    );
+  let emptyBoxes = Array.from(document.querySelectorAll(".box")).filter(
+    (box) =>
+      !box.classList.contains("circle") && !box.classList.contains("cross")
+  );
 
-    if (emptyBoxes.length === 0){
-        return;
-    } else {
-        let randomIndex = Math.floor(Math.random() * emptyBoxes.length);
-        game.gameLogicChecker();
-        console.log(emptyBoxes)
-        emptyBoxes[randomIndex].classList.add('circle');
-    }
+  if (emptyBoxes.length === 0) {
+    return;
+  } else {
+    let randomIndex = Math.floor(Math.random() * emptyBoxes.length);
+    game.gameLogicChecker();
+    console.log(emptyBoxes);
+    emptyBoxes[randomIndex].classList.add("circle");
+  }
 }
 
 box_wrapper.addEventListener("click", (e) => {
-
-    if (e.target.classList.contains("box")){
-
-    }
-})
-
-
-
-
-
+  if (e.target.classList.contains("box")) {
+  }
+});
